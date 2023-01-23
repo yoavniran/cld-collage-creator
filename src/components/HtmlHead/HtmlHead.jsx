6567055b4d5154ef364a96113d26bac74adfc538
yoavniran <yoavniran@gmail.com>
2023-01-23 11:11:55 +0200

    add og tags to index html

--- src/components/HtmlHead/HtmlHead.jsx
+++ src/components/HtmlHead/HtmlHead.jsx
@@ -6,17 +6,6 @@ const HtmlHead = ({ children }) => {
 		<HelmetProvider>
 			<Helmet>
 				<link rel="icon" type="image/png" href={favicon} sizes="192x192"/>
-				<meta property="og:title" content="Cloudinary Collage Creator by Yoav Niran" />
-				<meta property="og:type" content="website" />
-
-				<meta property="og:image" content="/ccc-og.png" />
-				<meta
-					name="description"
-					content="Create a collage from your photos using Cloudinary"
-				/>
-				<meta name="keywords" content="images,photos,collage,tool,cloudinary,create" />
-				<meta name="twitter:card" content="summary_large_image" />
-				<meta property="twitter:title" content="Cloudinary Collage Creator by Yoav Niran" />
 			</Helmet>
 			{children}
 		</HelmetProvider>
